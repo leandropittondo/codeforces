a63814a1fdceddce7df58c9f52967da5eb444e61
leandro pittondo <leandropittondo@gmail.com>
2025-07-26 10:22:26 -0300

    solved 112a problem

---
+++ 800/04-112A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::string s1, s2;
+    std::cin >> s1 >> s2;
+
+    for (char &c : s1)
+    {
+        c = tolower(c);
+    }
+
+    for (char &c : s2)
+    {
+        c = tolower(c);
+    }
+
+    if (s1 < s2)
+        cout << -1 << endl;
+    else if (s1 > s2)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
